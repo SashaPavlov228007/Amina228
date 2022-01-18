4077618049a42aca3dda79171f688c59f849b3de
SashaPavlov228007 <97970630+SashaPavlov228007@users.noreply.github.com>
2022-01-18 18:35:38 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+window.onload = () => 
+{
+    const BtnClick1 = document.getElementById("ButtonClick1");
+
+    BtnClick1.addEventListener("click", function ()
+    {
+        alert('Я тебя люблю');
+    });
+}
+
+function ButtonClick1()
+{
+    alert('Я тебя люблю');
+}
